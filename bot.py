bae68563ed2d8170e6d2e24953a41343f1cb1615
MakuninIII <imac.55505@gmail.com>
2022-12-20 17:29:57 +0300

    111

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from typing import List, Union
 import cmath
 import sympy
@@ -7,6 +8,7 @@ import math
 bot = telebot.TeleBot("2007989606:AAFtUMvumZFDbUKMigNDrq9y1ZpkcrFUDlY")
 
 state = {}
+
 def round_complex(x):
     """
     Функция окргуляет вещественную и мнимую часть комплексного числа до двух знаков после запятой и убирает скобки вокруг числа
@@ -24,11 +26,13 @@ def start(m: telebot.types.Message) -> telebot.types.Message:
     """
     markup = telebot.types.InlineKeyboardMarkup()
     markup.row_width = 1
-    items = (telebot.types.InlineKeyboardButton("Решение квадратных уравнений", callback_data="quad"), \
-         telebot.types.InlineKeyboardButton("Решение тригонометрических уравнений", callback_data="trigonometry"), \
-            telebot.types.InlineKeyboardButton("Решение интегральных уравнений", callback_data="integral"))
+    items = (telebot.types.InlineKeyboardButton("Решение квадратных уравнений", callback_data="quad"),
+         telebot.types.InlineKeyboardButton("Решение тригонометрических уравнений", callback_data="trigonometry"),
+            telebot.types.InlineKeyboardButton("Решение интегральных уравнений", callback_data="integral")
+            )         
     markup.add(*items)
-    return bot.send_message(chat_id=m.chat.id, text="Выбери режим", reply_markup=markup)
+    bot.send_message(chat_id=m.chat.id, text="Выбери режим", reply_markup=markup)
+    return 1
 
 
 @bot.callback_query_handler(func=lambda query: True)
@@ -40,104 +44,113 @@ def query_handler(call: telebot.types.CallbackQuery):
     """
     state[call.from_user.id] = call.data
     if call.data == 'quad':
-        return bot.send_message(chat_id=call.message.chat.id, text=f"режим изменен на {call.data}, введите коэффициенты через пробел")
+        bot.send_message(chat_id=call.message.chat.id,
+                            text=f"режим изменен на {call.data}, введите коэффициенты через пробел")
+        return 1
     elif call.data == 'trigonometry':
-        return bot.send_message(chat_id=call.message.chat.id, \
-             text=f"режим изменен на {call.data}, введите угол и тригонометрическую фукнцию через запятую и пробел")
+        bot.send_message(chat_id=call.message.chat.id,
+                             text=f"режим изменен на {call.data} введите угол и тригонометрическую фукнцию через запятую и пробел")
+        return 1          
     else:
-        return bot.send_message(chat_id=call.message.chat.id, \
+        bot.send_message(chat_id=call.message.chat.id,
              text=f"режим изменен на {call.data}, введите функцию, которая будет интегрирована, а также нижний и верхний пределы через пробелы")
+        return 1
 
 @bot.message_handler(func=lambda message: True)
 def calculator(message: telebot.types.Message):
     """
-    Вычисляет решение для заданного уравнения на основе текущего состояния и пользовательского ввода.
+    Вычисляет решение для заданного уравнения на основе текущего состояния и пользовательского ввода. 
     :param message: Объект сообщения, полученный от пользователя.
     :return: Cообщение, отправленное пользователю с решением уравнения.
     """
-    current_state = state.get(message.from_user.id, None)
-    if current_state == 'quad':
-        text = message.text
-        kf = text.split()
-        if len(kf) != 3:
-            return bot.send_message(chat_id=message.chat.id, text="введите 3 числа")
-        try:
-            kf = list(map(float, kf))
-        except Exception:
-            return bot.send_message(chat_id=message.chat.id, text="введите числа")
-        if kf[0] == 0 or kf[1] == 0 or kf[2] == 0:
-            return bot.send_message(chat_id=message.chat.id, text="коэффицент не должен быть равен нулю")
-
-        a, b, c = kf
-        D = int(b ** 2) - (4 * a * c)
-
-    
-        if D < 0:
-            x1 = round_complex((-b + cmath.sqrt(D)) / (2*a))
-            x2 = round_complex((-b - cmath.sqrt(D)) / (2*a))    
-            return bot.send_message(chat_id=message.chat.id, text=f"Нет вещественных корней, но есть комплексные:  \
-                \n\nПервый корень = {x1}\nВторой корень = {x2}")
-
-        if D == 0:
-            x = -b / 2 * a
-            return bot.send_message(chat_id=message.chat.id, text=f"один корень: {int(x)}")
-
-        x1: float = (-b - D ** 0.5) / (2 * a)
-        x2: float = (-b + D ** 0.5) / (2 * a)
-
-        if str(x1).endswith('.0'):
-            x1 = int(x1)
-        if str(x2).endswith('.0'):
-            x2 = int(x2)    
-        return bot.send_message(chat_id=message.chat.id, text=f'Дискриминант = *{D}*\n\n *Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}', parse_mode='Markdown')
-    elif current_state == 'trigonometry':
-        text = message.text.split(", ")
-        if len(text) != 2:
-            return bot.send_message(chat_id=message.chat.id, text=f'введите "угол, функция"')
-        angle_in_degrees = float(text[0])
-        trig_function = text[1]
-
-        # def trig_calculator(angle_in_degrees, trig_function):
-        #     # Convert the angle to radians
-        angle_in_radians = math.radians(angle_in_degrees)
-            
-        if trig_function == "sin":
-            result = math.sin(angle_in_radians)
-        elif trig_function == "cos":
-            result = math.cos(angle_in_radians)
-        elif trig_function == "tan" or trig_function == "tg":
-            result = math.tan(angle_in_radians)
-        elif trig_function == "cot" or trig_function == "ctg":
-            result = 1/(math.tan(angle_in_radians))
-        else:
-           return bot.send_message(chat_id=message.chat.id, text="Неверный ввод функции")
-            
-        return bot.send_message(chat_id=message.chat.id, text=f"{trig_function}(x) = {format(result, '.1f')}")   
-    
-    elif current_state == 'integral':
-        try:
+    try:
+        current_state = state.get(message.from_user.id, None)
+        if current_state == 'quad':
             text = message.text
-            parts = text.split()
-            if len(parts) != 3:
-                raise ValueError("Неверный ввод. Введите через пробел функцию, нижний и верхний пределы")
-            function = parts[0]
-            lower_limit = parts[1]
-            upper_limit = parts[2]
+            kf = text.split()
+            if len(kf) != 3:
+                return bot.send_message(chat_id=message.chat.id, text="введите 3 числа")
+            try:
+                kf = list(map(float, kf))
+            except Exception:
+                return bot.send_message(chat_id=message.chat.id, text="введите числа")
+            if kf[0] == 0 or kf[1] == 0 or kf[2] == 0:
+                return bot.send_message(chat_id=message.chat.id, text="коэффицент не должен быть равен нулю")
 
-            x = sympy.Symbol('x')
-            function = sympy.sympify(function)
-            lower_limit = sympy.sympify(lower_limit)
-            upper_limit = sympy.sympify(upper_limit)
+            a, b, c = kf
+            D = int(b ** 2) - (4 * a * c)
 
-            result = sympy.integrate(function, (x, lower_limit, upper_limit))
+        
+            if D < 0:
+                x1 = round_complex((-b + cmath.sqrt(D)) / (2*a))
+                x2 = round_complex((-b - cmath.sqrt(D)) / (2*a))    
+                bot.send_message(chat_id=message.chat.id, text=f"Нет вещественных корней, но есть комплексные:  \
+                    \n\nПервый корень = {x1}\nВторой корень = {x2}")
+                return 1
+            if D == 0:
+                x = -b / 2 * a
+                return bot.send_message(chat_id=message.chat.id, text=f"один корень: {int(x)}")
 
-            bot.send_message(message.chat.id, result)
-    
-        except ValueError:
-            bot.send_message(message.chat.id, "Ошибка ввода. Пожалуйста, введите действительные математическое выражение и пределы")
+            x1: float = (-b - D ** 0.5) / (2 * a)
+            x2: float = (-b + D ** 0.5) / (2 * a)
 
-    else:
-        return bot.send_message(chat_id=message.chat.id, text="Неверно")
+            if str(x1).endswith('.0'):
+                x1 = int(x1)
+            if str(x2).endswith('.0'):
+                x2 = int(x2)    
+            return bot.send_message(chat_id=message.chat.id, text=f'Дискриминант = *{D}*\n\n *Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}', parse_mode='Markdown')
+        elif current_state == 'trigonometry':
+            text = message.text.split(", ")
+            if len(text) != 2:
+                return bot.send_message(chat_id=message.chat.id, text=f'введите "угол, функция"')
+            angle_in_degrees = float(text[0])
+            trig_function = text[1]
+
+            # def trig_calculator(angle_in_degrees, trig_function):
+            #     # Convert the angle to radians
+            angle_in_radians = math.radians(angle_in_degrees)
+                
+            if trig_function == "sin":
+                result = math.sin(angle_in_radians)
+            elif trig_function == "cos":
+                result = math.cos(angle_in_radians)
+            elif trig_function == "tan" or trig_function == "tg":
+                result = math.tan(angle_in_radians)
+            elif trig_function == "cot" or trig_function == "ctg":
+                result = 1/(math.tan(angle_in_radians))
+            else:
+                return bot.send_message(chat_id=message.chat.id, text="Неверный ввод функции")
+
+            bot.send_message(chat_id=message.chat.id, text=f"{trig_function}(x) = {format(result, '.1f')}")   
+            return 1
+        
+        elif current_state == 'integral':
+            try:
+                text = message.text
+                parts = text.split()
+                if len(parts) != 3:
+                    raise ValueError("Неверный ввод. Введите через пробел функцию, нижний и верхний пределы")
+                function = parts[0]
+                lower_limit = parts[1]
+                upper_limit = parts[2]
+
+                x = sympy.Symbol('x')
+                function = sympy.sympify(function)
+                lower_limit = sympy.sympify(lower_limit)
+                upper_limit = sympy.sympify(upper_limit)
+
+                result = sympy.integrate(function, (x, lower_limit, upper_limit))
+
+                bot.send_message(message.chat.id, result)
+        
+            except ValueError:
+                bot.send_message(message.chat.id, "Ошибка ввода. Пожалуйста, введите действительные математическое выражение и пределы")
+
+        else:
+            return bot.send_message(chat_id=message.chat.id, text="Неверно")
+    except:
+        pass
+    return 1
 
 bot.polling(non_stop=True)
     
