43afbbf00f8b14f011e7096fcaea0e0783bdbc5c
MakuninIII <imac.55505@gmail.com>
2022-12-23 13:03:46 +0300

    123

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ bot = telebot.TeleBot("2007989606:AAFtUMvumZFDbUKMigNDrq9y1ZpkcrFUDlY")
 
 state = {}
 
+
 def round_complex(x):
     """
     Функция окргуляет вещественную и мнимую часть комплексного числа до двух знаков после запятой и убирает скобки вокруг числа
@@ -19,11 +20,12 @@ def round_complex(x):
     """
     return "{:.2f}{:+.2f}j".format(x.real, x.imag)
 
+
 @bot.message_handler(commands=["start"])
 def start(m: telebot.types.Message) -> telebot.types.Message:
     """
     Создает пять кнопок, которые позволяют пользователю выбрать режим для калькулятора, и отправляет сообщение с просьбой выбрать один из них.а
-    :param m: данные пользователя
+    :param m: сообщение от пользователя
     :return: 5 кнопок с различными режимами калькулятора и просит выбрать один из режимов
     """
     markup = telebot.types.InlineKeyboardMarkup()
@@ -67,6 +69,7 @@ def query_handler(call: telebot.types.CallbackQuery):
              text=f"режим изменен на {call.data}, введите функцию, которая будет интегрирована, а также нижний и верхний пределы через пробелы")
         return 1
 
+
 @bot.message_handler(func=lambda message: True)
 def calculator(message: telebot.types.Message):
     """
@@ -74,6 +77,82 @@ def calculator(message: telebot.types.Message):
     :param message: Объект сообщения, полученный от пользователя.
     :return: Cообщение, отправленное пользователю с решением уравнения.
     """
+    
+    def functquad(a,b,c):
+        """
+        Функция рисует график квадратичной функции
+        :param a: коэффициент при x^2
+        :param b: коэффициент при x
+        :param c: свободный член
+        :return: график квадратичной функции
+        """
+        x = np.arange(-10, 10, 0.1)
+        y = a*x**2 + b*x + c
+        plt.plot(x,y)
+        plt.grid(True)
+        plt.title('Quadratic Function')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        return plt.savefig('quadratic.png')
+
+    def functcub(a,b,c,d):
+        """
+        Функция рисует график кубической функции
+        :param a: коэффициент при x^3
+        :param b: коэффициент при x^2
+        :param c: коэффициент при x
+        :param d: свободный член
+        :return: график кубической функции
+        """
+        x = np.arange(-10, 10, 0.1)
+        y = a*x**3 + b*x**2 + c*x + d
+        plt.plot(x,y)
+        plt.grid(True)
+        plt.title('Cubic Function')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        return plt.savefig('cubic.png')
+
+    def functquart(a,b,c,d,e):
+        """
+        Функция рисует график биквадратрой функции
+        :param a: коэффициент при x^4
+        :param b: коэффициент при x^3
+        :param c: коэффициент при x^2
+        :param d: коэффициент при x
+        :param e: свободный член
+        :return: график квартической функции
+        """
+        x = np.arange(-10, 10, 0.1)
+        y = a*x**4 + b*x**3+ c*x**2 + d*x + e
+        plt.plot(x,y)
+        plt.grid(True)
+        plt.title('Biquadratic Function')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        return plt.savefig('cubic.png')
+
+    def functintegral(function, lower, upper):
+        """
+        Функция вычисляет определенный интеграл
+        :param function: функция
+        :param lower: нижний предел интегрирования
+        :param upper: верхний предел интегрирования
+        :return: графическое отображение определенного интеграла
+        """
+        function = eval('lambda x: ' + function)
+        lower = float(lower)
+        upper = float(upper)
+        x = np.arange(lower, upper+0.1, 0.1)
+        y = function(x)
+        plt.plot(x,y)
+        plt.fill_between(x, y)
+        plt.grid(True)
+        plt.title('Integral')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        return plt.savefig('integral.png')
+
     current_state = state.get(message.from_user.id, None)
     if current_state == 'quad':
         text = message.text
@@ -89,14 +168,7 @@ def calculator(message: telebot.types.Message):
 
         a, b, c = kf
         D = int(b ** 2) - (4 * a * c)
-
-        x = np.arange(-10, 10, 0.1)
-        y = a*x**2 + b*x + c
-        plt.plot(x,y)
-        plt.title('Quadratic Function')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.savefig('quadratic.png')
+        functquad(a,b,c)
     
         if D < 0:
             x1 = round_complex((-b + cmath.sqrt(D)) / (2*a))
@@ -118,9 +190,12 @@ def calculator(message: telebot.types.Message):
             x1 = int(x1)
         if str(x2).endswith('.0'):
             x2 = int(x2)    
-        bot.send_message(chat_id=message.chat.id, text=f'Дискриминант = *{D}*\n\n *Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}', parse_mode='Markdown')
+        
+        bot.send_message(chat_id=message.chat.id,
+             text=f'Дискриминант = *{D}*\n\n *Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}', parse_mode='Markdown')
         bot.send_photo(chat_id=message.chat.id, photo=open('quadratic.png', 'rb'))
         return 1
+
 
     elif current_state == 'trigonometry':
         text = message.text.split(", ")
@@ -129,8 +204,6 @@ def calculator(message: telebot.types.Message):
         angle_in_degrees = float(text[0])
         trig_function = text[1]
 
-        # def trig_calculator(angle_in_degrees, trig_function):
-        #     # Convert the angle to radians
         angle_in_radians = math.radians(angle_in_degrees)
             
         if trig_function == "sin":
@@ -144,6 +217,7 @@ def calculator(message: telebot.types.Message):
         else:
             return bot.send_message(chat_id=message.chat.id, text="Неверный ввод функции")
 
+
         bot.send_message(chat_id=message.chat.id, text=f"{trig_function}(x) = {format(result, '.1f')}")   
         return 1
     
@@ -156,18 +230,22 @@ def calculator(message: telebot.types.Message):
             function = parts[0]
             lower_limit = parts[1]
             upper_limit = parts[2]
+            functintegral(function, lower_limit, upper_limit)
 
             x = sympy.Symbol('x')
             function = sympy.sympify(function)
             lower_limit = sympy.sympify(lower_limit)
             upper_limit = sympy.sympify(upper_limit)
-
+            
             result = sympy.integrate(function, (x, lower_limit, upper_limit))
 
-            bot.send_message(message.chat.id, result)
+            bot.send_message(message.chat.id, text=f"Интергал равен {result}")
+            bot.send_photo(chat_id=message.chat.id, photo=open('integral.png', 'rb'))
+            return 1
     
         except ValueError:
             bot.send_message(message.chat.id, "Ошибка ввода. Пожалуйста, введите действительные математическое выражение и пределы")
+            return 1
 
     if current_state == 'cubic':
         text = message.text
@@ -178,28 +256,22 @@ def calculator(message: telebot.types.Message):
             kf = list(map(float, kf))
         except Exception:
             return bot.send_message(chat_id=message.chat.id, text="введите числа")
-        if kf[0] == 0 or kf[1] == 0 or kf[2] == 0:
-            return bot.send_message(chat_id=message.chat.id, text="коэффицент не должен быть равен нулю")
-
+        
         a, b, c, d= kf
-        p = b - a**2 / 3
-        q = 2 * a**3 / 27 - a * b / 3 + c
-        r = cmath.sqrt(-q/2 + cmath.sqrt(q**2/4 + p**3/27))
-        s = cmath.sqrt(-q/2 - cmath.sqrt(q**2/4 + p**3/27))
-
-        # x = np.arange(-10, 10, 0.1)
-        # y = a*x**3 + b*x**2 + c*x + d
-        # plt.plot(x,y)
-        # plt.title('Quadratic Function')
-        # plt.xlabel('x')
-        # plt.ylabel('y')
-        # plt.savefig('cubic.png')
-    
-        x1 = r + s - a/3
-        x2 = -(r + s)/2 - a/3 + math.sqrt(3)/2 * (r - s) * 1j
-        x3 = -(r + s)/2 - a/3 - math.sqrt(3)/2 * (r - s) * 1j
-        bot.send_message(chat_id=message.chat.id, text=f'*Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}\nТретий корень = {format(x3,".3f")}', parse_mode='Markdown')
-        # bot.send_photo(chat_id=message.chat.id, photo=open('cubic.png', 'rb'))
+        functcub(a,b,c,d)
+        x1, x2, x3 = np.roots(kf)
+        if x1.imag == 0:
+            x1 = x1.real
+        if x2.imag == 0:
+            x2 = x2.real
+        if x3.imag == 0:
+            x3 = x3.real
+        
+       
+        bot.send_message(chat_id=message.chat.id,
+             text=f'*Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}\nТретий корень = {format(x3,".3f")}',
+                 parse_mode='Markdown')
+        bot.send_photo(chat_id=message.chat.id, photo=open('cubic.png', 'rb'))
         return 1
 
     if current_state == 'quartic':
@@ -211,30 +283,19 @@ def calculator(message: telebot.types.Message):
             kf = list(map(float, kf))
         except Exception:
             return bot.send_message(chat_id=message.chat.id, text="введите числа")
-        if kf[0] == 0 or kf[1] == 0 or kf[2] == 0:
-            return bot.send_message(chat_id=message.chat.id, text="коэффицент не должен быть равен нулю")
-
+    
         a, b, c, d, e= kf
-        p = 8 * b - 3 * a**2
-        q = a**3 - 4 * a * b + 8 * c
-        r = cmath.sqrt(-q/2 + cmath.sqrt(q**2/4 + p**3/27))
-        s = cmath.sqrt(-q/2 - cmath.sqrt(q**2/4 + p**3/27))
+        functquart(a,b,c,d,e)
+        x1, x2, x3, x4 = np.roots(kf)
 
-        x = np.arange(-10, 10, 0.1)
-        y = a*x**4 + b*x**3 + c*x*2 + d*x + e
-        plt.plot(x,y)
-        plt.title('Quadratic Function')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.savefig('quartic.png')
-    
-        u = r + s
-        v = math.sqrt(3) * (r - s)
-    
-        x1 = cmath.sqrt(-a/4 + u/2 + v/2) + cmath.sqrt(-a/4 + u/2 - v/2)
-        x2 = cmath.sqrt(-a/4 + u/2 + v/2) - cmath.sqrt(-a/4 + u/2 - v/2)
-        x3 = -cmath.sqrt(-a/4 + u/2 + v/2) + cmath.sqrt(-a/4 + u/2 - v/2) * 1j
-        x4 = -cmath.sqrt(-a/4 + u/2 + v/2) - cmath.sqrt(-a/4 + u/2 - v/2) * 1j
+        if x1.imag == 0:
+            x1 = x1.real
+        if x2.imag == 0:
+            x2 = x2.real
+        if x3.imag == 0:
+            x3 = x3.real
+        if x4.imag == 0:
+            x4 = x4.real
 
         bot.send_message(chat_id=message.chat.id,
             text=f'*Корни:*\nПервый корень = {format(x1,".3f")}\nВторой корень = {format(x2, ".3f")}\nТретий корень = {format(x3,".3f")}\nЧетвёртый корень = {format(x4,".3f")}',\
@@ -245,9 +306,6 @@ def calculator(message: telebot.types.Message):
 
     else:
             return bot.send_message(chat_id=message.chat.id, text="Неверно")
-    # except:
-    #     pass
-    # return 1
 
 bot.polling(non_stop=True)
     
